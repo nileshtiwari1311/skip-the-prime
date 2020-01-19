3e69efdef087afe4814648c690fc714577040fd5
CS17B022 <cs17b022@iittp.ac.in>
2020-01-20 00:22:07 +0530

    First commit

---
+++ run/Assets/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+    public float timeDecrease;
+    public float minTime;
+
+    public GameObject[] obstaclePatterns;
+
+    private void Start()
+    {
+        timeBtwSpawns = startTimeBtwSpawns;
+    }
+
+    private void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            int rand = Random.Range(0, obstaclePatterns.Length);
+            Instantiate(obstaclePatterns[rand], transform.position, Quaternion.identity);
+            timeBtwSpawns = startTimeBtwSpawns;
+           
+            if (startTimeBtwSpawns > minTime)
+            {
+                startTimeBtwSpawns -= timeDecrease;
+            }
+        }
+        else
+        {
+            
+            timeBtwSpawns -= Time.deltaTime;
+        }
+    }
+
+}
